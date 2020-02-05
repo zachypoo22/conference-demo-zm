9ccdb2dbcb2f50c904a6c73c6070a69357fdc98a
Zacheriah <zachm93@live.com>
2020-02-05 13:41:55 -0500

    first

--- src/main/java/com/pluralsite/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluralsite/conferencedemo/config/PersistenceConfiguration.java
@@ -10,17 +10,17 @@ import java.util.Map;
 @Configuration
 public class PersistenceConfiguration {
 
-    @Bean
-    public DataSource dataSource(){
-
-        Map<String, String> EnvMap = System.getenv();
-
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url(EnvMap.get("DB_URL"));
-        builder.password(EnvMap.get("DB_PW"));
-        builder.username(EnvMap.get("DB_UN"));
-        System.out.println("Custom bean init / set");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//
+//        Map<String, String> EnvMap = System.getenv();
+//
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url(EnvMap.get("DB_URL"));
+//        builder.password(EnvMap.get("DB_PW"));
+//        builder.username(EnvMap.get("DB_UN"));
+//        System.out.println("Custom bean init / set");
+//        return builder.build();
+//    }
 
 }
